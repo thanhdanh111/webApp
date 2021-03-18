4d867e276918abfcc3f89f517fa14186fe8edb9c
Linh Hứa Thị <linhhuacntt@gmail.com>
2021-03-18 08:18:42 +0000

    [#6kr9br] Feat/create manager users list

---
+++ pages/users/UI/user_link_menu.tsx
@@ -0,0 +1,38 @@
+import { Container, Typography } from '@material-ui/core';
+import React from 'react';
+
+const UserLinkMenu = () => {
+  return (
+            <div className='header-list'>
+                <Typography className='title-list' component='h3' variant='h4' align='left' color='textPrimary'>
+                    User List
+                </Typography>
+                <Container className='link-list'>
+                    <ul className='link-list-ul'>
+                        <li className='link-item item-first'>
+                            <div className='item'>
+                                <a href='/dashboard' className='link'>Dashboard</a>
+                            </div>
+                        </li>
+                        <li className='link-item'>
+                            <div className='item'>
+                                <a href='/manager' className='link'>Management</a>
+                            </div>
+                        </li>
+                        <li className='link-item'>
+                            <div className='item'>
+                                <a href='/user' className='link'>User</a>
+                            </div>
+                        </li>
+                        <li className='link-item'>
+                            <div className='item'>
+                                <a href='/users' className='link'>List</a>
+                            </div>
+                        </li>
+                    </ul>
+                </Container>
+            </div>
+  );
+};
+
+export default(UserLinkMenu);
