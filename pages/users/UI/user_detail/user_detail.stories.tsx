6a21da3b22492d33e32ad916b21fce48d49342be
Lê Hoàng Uyên <7339448-LeHoangUyen96@users.noreply.gitlab.com>
2021-05-20 10:22:28 +0000

    [CU-8fxyug] Feat/expanded user info

--- pages/users/UI/user_detail/user_detail.stories.tsx
+++ pages/users/UI/user_detail/user_detail.stories.tsx
@@ -1,9 +1,9 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
-import HeadTable from './head_table';
 
 storiesOf('BaseTable', module).add('BaseTable', () => {
   return (
-  <HeadTable headCells={[]} needCheckBox={false}  hadExpandableRows={false}/>
+    <>
+    </>
   );
 });
