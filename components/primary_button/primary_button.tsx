5c440bad2d785a3d17ab584350884f70b8f40b94
Linh Hứa Thị <linhhuacntt@gmail.com>
2021-05-27 06:01:28 +0000

    [#8kwuh9] Feat/check role in company

--- components/primary_button/primary_button.tsx
+++ components/primary_button/primary_button.tsx
@@ -6,14 +6,15 @@ import {
 interface DataType {
   title: string;
   handleClick: () => void;
+  extendClass?: string;
 }
 
 type BodyProps = DataType;
 
-const PrimaryButtonUI: FunctionComponent<BodyProps> = ({ title, handleClick }) => {
+const PrimaryButtonUI: FunctionComponent<BodyProps> = ({ title, handleClick, extendClass }) => {
 
   return (
-    <div className='btn-primary-container'>
+    <div className={`btn-primary-container ${extendClass}`}>
       <Button color='secondary' variant='contained' onClick={handleClick} className='btn-primary' >
         {title}
       </Button>
