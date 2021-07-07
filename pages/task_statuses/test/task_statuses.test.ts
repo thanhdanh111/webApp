c93beb0ecdee3505a5321a3e3a4a352afa5bfe99
Linh Hứa Thị <linhhuacntt@gmail.com>
2021-07-07 10:26:31 +0700

    update

--- pages/task_statuses/test/task_statuses.test.ts
+++ pages/task_statuses/test/task_statuses.test.ts
@@ -32,26 +32,32 @@ beforeAll(async () => {
 });
 
 describe('Home page', () => {
-  test('Test taskStatus successfully after login', async () => {
+  test('Test get taskStatuses successfully after login', async () => {
     await page.goto('http://localhost:5000/home');
     await page.waitForSelector('.board');
-
-    const image = await page.screenshot();
-    expect(image).toMatchImageSnapshot();
-
-
     await page.waitForSelector('.status');
 
     const status = await page.screenshot();
     expect(status).toMatchImageSnapshot();
+  });
+
+  test('Test add taskStatus successfully after login', async () => {
+    await page.goto('http://localhost:5000/home');
+    await page.waitForSelector('.board');
+    await page.waitForSelector('.status');
 
     await page.waitForSelector('.add-task-text');
     await page.click('.add-task-text');
-
     await page.waitForSelector('.add-status-modal');
 
     const addStatus = await page.screenshot();
     expect(addStatus).toMatchImageSnapshot();
+  });
+
+  test('Test actions taskStatus successfully after login', async () => {
+    await page.goto('http://localhost:5000/home');
+    await page.waitForSelector('.board');
+    await page.waitForSelector('.status');
 
     await page.waitForSelector('.action-status-btn');
     await page.click('.action-status-btn');
@@ -69,8 +75,24 @@ describe('Home page', () => {
 
     const actionRetitleStatus = await page.screenshot();
     expect(actionRetitleStatus).toMatchImageSnapshot();
+  });
+  test('Test actions taskStatus successfully after login', async () => {
+    await page.goto('http://localhost:5000/home');
+    await page.waitForSelector('.board');
+    await page.waitForSelector('.status');
 
+    await page.waitForSelector('.action-status-btn');
+    await page.click('.action-status-btn');
 
+    await page.waitForSelector('.popper-action-status');
+    await page.waitFor(5000);
+    await page.click('.rename-status-menu-item');
+
+    await page.waitForSelector('.add-status-input');
+    await page.waitFor(5000);
+
+    const actionRetitleStatus = await page.screenshot();
+    expect(actionRetitleStatus).toMatchImageSnapshot();
   });
 
 });
