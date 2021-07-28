806d24d91734c0c5c71b3c2782c85ca14962865a
TranThanhDanh <tranthanhdanh0311@gmail.com>
2021-07-28 12:13:14 +0700

    update test

--- pages/task_comment/test/task_comment.test.ts
+++ pages/task_comment/test/task_comment.test.ts
@@ -34,14 +34,48 @@ describe('Home page', () => {
   test('Test home successfully after login', async () => {
     await page.goto('http://localhost:5000/home')
     await page.waitForSelector('.board')
-    await page.waitForSelector('.board-tasks')
-    await page.waitForSelector('.task-status')
-    await page.waitForSelector('.status-task-list')
-    await page.click('.status-task-list .task-item')
-    await page.waitForSelector('.task-comment-ui')
+    await page.waitForSelector('.status')
+  // add statuss open
+    await page.waitForSelector('.add-task-text')
+    await page.click('.add-task-text')
+    await page.waitForSelector('.add-status-modal')
 
-    const taskComment = await page.screenshot()
-    expect(taskComment).toMatchImageSnapshot()
+    await page.click('.add-status-input')
+    await page.type('.add-status-input', 'open')
+
+    await page.click('.submit-create-status')
+    await page.click('.close-create-status')
+  // add status change status
+    await page.click('.add-task-text')
+    await page.waitForSelector('.add-status-modal')
+
+    await page.click('.add-status-input')
+    await page.type('.add-status-input', 'change status')
+
+    await page.click('.submit-create-status')
+    await page.click('.close-create-status')
+  // add task
+    await page.waitForSelector('.open .add-task')
+
+    await page.click('.open .add-task')
+    await page.waitForSelector('.task-add')
+
+    await page.waitForSelector('input[name=title]')
+
+    await page.click('input[name=title]')
+    await page.type('input[name=title]', 'feat/created task for task')
+
+    await page.waitForSelector('.save-add')
+    await page.click('.save-add')
+
+  // task detail
+    await page.waitFor(5000)
+    await page.click('.task-name')
+    await page.waitForSelector('.detail-modal')
+
+    const taskDetail = await page.screenshot()
+    expect(taskDetail).toMatchImageSnapshot()
+
   })
 })
 
